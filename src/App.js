cb9125b0c44be7c265429cb598371b3b7d6f7559
kingbaddo <wizzyblaq94@gmail.com>
2021-06-13 07:53:16 +0100

    pull data from tmdb

--- src/App.js
+++ src/App.js
@@ -6,9 +6,19 @@ import requests from './requests';
 function App() {
   return (
     <div className="App">
-     <h1>Hello Web</h1>
-     <RowPost title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals}/>
+
+     
+     <RowPost title="BESFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals}
+     isLargeRow={true}/>
+
      <RowPost title="Trending Now" fetchUrl={requests.fetchTrending}/>
+     <RowPost title="Top Rated"    fetchUrl={requests.fetchTopRated}/>
+     <RowPost title="Action Movies" fetchUrl={requests.fetchActionMovies}/>
+     <RowPost title="Comedy Movies" fetchUrl={requests.fetchComedyMovies}/>
+      <RowPost title="Horror Movies" fetchUrl={requests.fecthHorrorMovies}/>
+      <RowPost title="Romance Movies" fetchUrl={requests.fetchRomanceMovies}/>
+      <RowPost title="Documentaries Movies" fetchUrl={requests.fetchDocumentaries}/>
+
 
     </div>
   );
--- src/components/Row.js
+++ src/components/Row.js
@@ -1,14 +1,40 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from '../axios';
+
+const base_url = "https://image.tmdb.org/t/p/original/";
+
+function RowPost({title, fetchUrl, isLargeRow}) {
+ const [ movies, setMovies ] = useState([]);
 
 
-function RowPost({title}) {
- const [ movies, setMovie ] = useState([])
+ //A snipet of code that  runs based on a specific condition
+  useEffect(()=> {
+     async function fetchData(){
+         const request = await axios.get(fetchUrl);
+         setMovies(request.data.results);
+         return request;
+         
+     }
+     fetchData();
+  }, [fetchUrl])
     return(
         <div className="row">
             <h2>{title}</h2>
-            <section className="movie-row container">
-
-            </section>
+            <div className="movies-row">
+                <div className="post-container">
+              {movies.map( movie => (
+                  
+                         
+                  <img key={movie.id}
+                  className={`row__poster ${isLargeRow && "row__posterLarge"} `}
+                  src={`${base_url}${isLargeRow ? movie.poster_path : movie.backdrop_path}`} alt={movie.name} />
+                 
+                  
+                  
+                  
+              ))}
+              </div>
+            </div>
 
         </div>
     )
